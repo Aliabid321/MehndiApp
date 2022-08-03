a05dd67be627aae40003972d5852f841d7ae5273
Ali Abid <alikhanbloch1233@gmail.com>
2022-08-03 20:05:28 +0500

    MehndiAPP intial creating right now

---
+++ app/src/main/java/com/example/mehndidesignapp/Activitise/SplashScreenActivity.java
@@ -0,0 +1,14 @@
+package com.example.mehndidesignapp.Activitise;
+
+import androidx.appcompat.app.AppCompatActivity;
+import com.example.mehndidesignapp.R;
+import android.os.Bundle;
+
+public class SplashScreenActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_splash_screen);
+    }
+}
